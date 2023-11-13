2104c3e3b5757ac7abb1d701abecf76f9e9ce118
JoseDev <joseijd69@gmail.com>
2023-11-13 14:19:15 -0500

    add components for dialogs

---
+++ src/app/components/dialogSuccess.tsx
@@ -0,0 +1,41 @@
+import {Dialog} from "primereact/dialog";
+import {Button} from "primereact/button";
+
+interface DialogSuccessProps {
+    visible: boolean
+    setVisible: (data: boolean) => void
+}
+
+
+export default function DialogSuccess(props: DialogSuccessProps) {
+    const {visible, setVisible} = props
+    return <Dialog visible={visible} style={{width: '35vw',}} onHide={() => setVisible(false)} draggable={false}
+                   pt={{root: {className: 'border-round-sm'}}}>
+        <div className="grid justify-content-center align-items-center align-content-center">
+            <p className="p-0 m-0 text-3xl text-center font-bold">
+                Address updated
+            </p>
+            <p className="m-4 text-center text-lg font-semibold">
+                New address added to your account
+            </p>
+            <p className="m-1 text-center font-medium text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                u llameo.
+
+            </p>
+            <p className="p-2 text-lg font-medium text-center">
+                Nisi ut aliquip ex ea commodo consequat.
+            </p>
+        </div>
+        <div className="flex flex-column">
+            <div className="flex h-6rem"></div>
+        </div>
+        <div className="flex justify-content-center align-items-center">
+            <div className="flex col-7">
+                <Button className="w-full bg-pink-400" rounded label="Understood"/>
+            </div>
+        </div>
+    </Dialog>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,71 @@
 'use client';
-import {Dialog} from 'primereact/dialog';
-import {Button} from "primereact/button";
-import {useState} from "react";
-import {AutoComplete, AutoCompleteCompleteEvent} from 'primereact/autocomplete';
-import {InputText} from "primereact/inputtext";
-import { ListBox, ListBoxChangeEvent } from 'primereact/listbox';
 
-interface Country {
-    name: string;
-    code: string;
-}
+import {useCallback, useEffect, useState} from "react";
+import {InputText} from "primereact/inputtext";
+import {ListBox, ListBoxChangeEvent} from 'primereact/listbox';
+import {useDispatch, useSelector} from "react-redux";
+import {bindActionCreators} from "redux";
+import {actionCreators} from "../../state";
+import {getCodes, getPlaceDetail, getPlaces, Prediction} from "../../api/GoogleAPI";
+import {AppState} from "../../state";
+import DialogSuccess from "@/app/components/dialogSuccess";
+import DialogFailure from "@/app/components/dialogFailure";
+
+
+
 export default function Home() {
-    const [visible, setVisible] = useState(false);
+    const [visibleSuccess, setVisibleSuccess] = useState(false);
+    const [visibleFailure, setVisibleFailure] = useState(false);
+    const [visibleList, setVisibleList] = useState(false);
     const [value, setValue] = useState<string>('');
-    const [items, setItems] = useState<string[]>([]);
-    const [selectedCountries, setSelectedCountries] = useState<Country | null>(null);
-    const countries: Country[] = [
-        { name: 'Australia', code: 'AU' },
-        { name: 'Brazil', code: 'BR' },
-        { name: 'China', code: 'CN' }
-    ];
-    const countryTemplate = (option: Country) => {
+    const [selectedPlace, setSelectedPlace] = useState<Prediction | null>(null);
+    const dispatch = useDispatch();
+    const {searchPlaces, getZipCodes, obtainPlaceDetail} = bindActionCreators(actionCreators, dispatch)
+    const state = useSelector((state: AppState) => state.app)
+
+    useEffect(() => {
+        getCodes().then((response) => {
+            getZipCodes(response)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
+    const filteredMostRelevant = () => {
+        return state.places.slice(0, 3)
+    }
+    const placeTemplate = (option: Prediction) => {
         return (
-            <div className="flex align-items-center">
-                <img alt={option.name} src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png" className={`flag flag-${option.code.toLowerCase()}`} style={{ width: '1.25rem', marginRight: '.5rem' }}/>
-                <div>{option.name}</div>
+            <div className="grid nested-grid">
+                <div className="flex col-1 align-items-center justify-content-center">
+                    <img alt="cursor" src="/images/map-pin-gray.png"
+                         style={{width: '1.25rem', marginRight: '.5rem'}}/>
+
+                </div>
+                <div className="col-11">
+                    <div className="font-light font-semibold mt-2 mb-2">{option.structured_formatting.main_text}</div>
+                    <div className="font-light text-base">{option.structured_formatting.secondary_text}</div>
+                </div>
             </div>
         );
     };
-    const search = (event: AutoCompleteCompleteEvent) => {
-        // @ts-ignore
-        setItems([...Array(10).keys()].map(item => event.query + '-' + item));
+
+    const selectPlace = (place: Prediction) => {
+        setValue(place.description)
+        setSelectedPlace(place)
+        setVisibleList(false)
+
+        getPlaceDetail(place.place_id).then((response) => {
+          
+        })
+
     }
+    const searchPlacesFunc = useCallback((place: string) => {
+        setValue(place)
+        getPlaces(place).then((response) => {
+            searchPlaces(response)
+        })
+        setVisibleList(true)
+    }, [state])
     return (
         <>
             <div className="flex flex-wrap h-screen p-0 m-0">
@@ -39,14 +73,14 @@ export default function Home() {
                     className="col-12 flex justify-content-center border-round">
                     <div className="flex flex-column  align-content-center">
                         <div>
-                            <p className="text-3xl text-center">
+                            <p className="text-3xl text-center font-bold">
                                 Where are you located?
                             </p>
                         </div>
-                        <div><p className="text-center">
+                        <div><p className="text-center font-semibold">
                             So we know where to drop off the stuff
                         </p></div>
-                        <div><p className="text-center">
+                        <div><p className="text-center ">
                             {/* eslint-disable-next-line react/no-unescaped-entities */}
                             We won't share you address
                             <br/>
@@ -56,17 +90,28 @@ export default function Home() {
 
                         </p></div>
                         <div className="flex justify-content-center align-content-center flex-wrap">
-                                    <span className="p-input-icon-left"><i className="pi pi-search"/>
-                                <InputText className="flex w-26rem" placeholder="Search"/>
+                                    <span className="p-input-icon-left">
+                                        <img src="/images/map-pin.png" width="17" height="19"
+                                             style={{position: 'absolute', zIndex: 2, marginTop: 15, left: 8}}/>
+                                <InputText className="flex w-26rem h-3rem" placeholder="Search" value={value}
+                                           onChange={(event) => searchPlacesFunc(event.target.value)}/>
                             </span>
 
                         </div>
-                        <ListBox value={selectedCountries} onChange={(e: ListBoxChangeEvent) => setSelectedCountries(e.value)} options={countries} optionLabel="name"
-                                 itemTemplate={countryTemplate} className="w-full" listStyle={{ maxHeight: '250px' }} />
-                    </div>
 
+                        {
+                            visibleList &&
+                            <ListBox value={selectedPlace?.description}
+                                     onChange={(e: ListBoxChangeEvent) => selectPlace(e.value)}
+                                     options={filteredMostRelevant()}
+                                     optionLabel="name"
+                                     itemTemplate={placeTemplate} className="w-full" listStyle={{maxHeight: '265px'}}/>
+                        }
+                    </div>
                 </div>
             </div>
+            <DialogSuccess visible={visibleSuccess} setVisible={setVisibleSuccess}/>
+            <DialogFailure visible={visibleFailure} setVisible={setVisibleFailure}/>
         </>
     )
 }
